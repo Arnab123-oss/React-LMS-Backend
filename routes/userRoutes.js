2714cebabf7dc0c97306120fc12b569f68faf2f5
Arnab Ghosh <ajg616725@gmail.com>
2023-03-01 19:32:50 +0530

    dfsdfvdfkjfvc

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,11 @@ import {
   register,
   logout,
   getMyProfile,
+  changePassword,
+  updateProfile,
+  updateProfilePicture,
+  forgetPassword,
+  resetPassword,
 } from "../controllers/userController.js";
 import { isAuthenticated } from "../middlewares/auth.js";
 
@@ -24,11 +29,26 @@ router.route("/logout").get(logout);
 router.route("/me").get(isAuthenticated, getMyProfile);
 
 // ChangePassword
+
+router.route("/changepassword").put(isAuthenticated, changePassword);
+
 // UpdateProfile
+
+router.route("/updateprofile").put(isAuthenticated, updateProfile);
+
 // UpdateProfilePicture
 
+router
+  .route("/updateprofilepicture")
+  .put(isAuthenticated, updateProfilePicture);
+
 // ForgetPassword
+
+router.route("/forgetpassword").post(forgetPassword);
+
 // ResetPassword
+
+router.route("/resetpassword/:token").put(resetPassword);
 
 //AddToPlayList
 //RemovePlayList
